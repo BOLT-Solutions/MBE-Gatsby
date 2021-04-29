023ce4f8d7bf14e6b271d88baae3c7a699d119ed
y.ghazy@boltsmartsolutions.com <y.ghazy@boltsmartsolutions.com>
2021-04-29 14:41:53 +0200

    style component css

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { graphql, Link, useStaticQuery } from "gatsby"
 import phpValidate from "../assets/vendor/php-email-form/validate.js";
-
+import '../components/style/ContactUs.css'
     export default function ContactUs() {
         const {allPrismicContactus} = useStaticQuery(graphql`
             query ContactUs {
@@ -48,7 +48,7 @@ import phpValidate from "../assets/vendor/php-email-form/validate.js";
                         </div>
 
                         <div className="row" data-aos="fade-up" data-aos-delay="100">
-                            <div className="col-lg-6">
+                            <div className="col-lg-4">
                                 <div className="info-box mb-4">
                                     <i className="bx bx-map"></i>
                                     <h3>Our Address</h3>
@@ -56,7 +56,7 @@ import phpValidate from "../assets/vendor/php-email-form/validate.js";
                                 </div>
                             </div>
 
-                            <div className="col-lg-3 col-md-6">
+                            <div className="col-lg-4 col-md-6">
                                 <div className="info-box  mb-4">
                                     <i className="bx bx-envelope"></i>
                                     <h3>Email Us</h3>
@@ -64,7 +64,7 @@ import phpValidate from "../assets/vendor/php-email-form/validate.js";
                                 </div>
                             </div>
 
-                            <div className="col-lg-3 col-md-6">
+                            <div className="col-lg-4 col-md-6">
                                 <div className="info-box  mb-4">
                                     <i className="bx bx-phone-call"></i>
                                     <h3>Call Us</h3>
@@ -79,12 +79,12 @@ import phpValidate from "../assets/vendor/php-email-form/validate.js";
 
                         <div className="row" data-aos="fade-up" data-aos-delay="100">
 
-                            <div className="col-lg-6 ">
+                            <div className="col-lg-4 ">
                                 <iframe className="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
                                     frameBorder="0" style={{ border: '0', width: '100%', height: '384px' }} allowFullScreen></iframe>
                             </div>
 
-                            <div className="col-lg-6">
+                            <div className="col-lg-8">
                                 <form action="forms/contact.php" method="post" role="form" className="php-email-form">
                                     <div className="form-row">
                                         <div className="col form-group">
