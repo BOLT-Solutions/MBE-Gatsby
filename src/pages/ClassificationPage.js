57af7cfce900235109a2b324431f2abfe5c04cbf
y.ghazy@boltsmartsolutions.com <y.ghazy@boltsmartsolutions.com>
2021-05-05 12:28:23 +0200

    classification header

--- src/pages/ClassificationPage.js
+++ src/pages/ClassificationPage.js
@@ -9,72 +9,90 @@ export default function ClassificationPage() {
         query ClassificationPage {
             allPrismicClassification {
                 edges {
-                  node {
-                    data {
-                      banner_image {
-                        url
-                      }
-                      classification_products {
-                        classfication_product_description {
-                          text
+                    node {
+                        data {
+                            banner_image {
+                                url
+                            }
+                            classification_products {
+                                classfication_product_description {
+                                    text
+                                }
+                                classification_product_title {
+                                    text
+                                }
+                                download_brochure_link {
+                                    url
+                                }
+                                request_information_link {
+                                    url
+                                }
+                                side_image {
+                                    url
+                                }
+                            }
+                            classification_products_title {
+                                text
+                            }
+                            description {
+                                text
+                            }
+                            icon {
+                                url
+                            }
+                            title {
+                                text
+                            }
                         }
-                        classification_product_title {
-                          text
-                        }
-                        download_brochure_link {
-                          url
-                        }
-                        request_information_link {
-                          url
-                        }
-                        side_image {
-                          url
-                        }
-                      }
-                      classification_products_title {
-                        text
-                      }
-                      description {
-                        text
-                      }
-                      icon {
-                        url
-                      }
-                      title {
-                        text
-                      }
                     }
-                  }
                 }
-              }
             }
-            
+        }
     `)
 
     let banner_image =
         allPrismicClassification.edges[0].node.data.banner_image.url
     let classification_products_title =
-        allPrismicClassification.edges[0].node.data.classification_products_title.text
+        allPrismicClassification.edges[0].node.data
+            .classification_products_title.text
     let description =
         allPrismicClassification.edges[0].node.data.description.text
     let icon = allPrismicClassification.edges[0].node.data.icon.url
     let title = allPrismicClassification.edges[0].node.data.title.text
-    let classification_products = allPrismicClassification.edges[0].node.data.classification_products
+    let classification_products =
+        allPrismicClassification.edges[0].node.data.classification_products
 
-    return <Layout>
-<div className="background" style={{ background: `url(${banner_image})`, backgroundSize:"cover",height:"60vh" }}></div>
+    return (
+        <Layout>
+            <div
+                className="background"
+                style={{
+                    background: `url(${banner_image})`,
+                    backgroundSize: "cover",
+                    height: "60vh",
+                }}
+            ></div>
             <section className="container">
-                <h1><strong>{title}</strong></h1>
+                <h1 style={{ color: "#FC9200" }}>
+                    <strong>{title}</strong>
+                </h1>
                 <div className="d-flex mt-5">
-                    <div className="imgContainer mr-5">
-
-                    <img src={icon}/>
+                    <div
+                        className="imgContainer mr-5"
+                        style={{ width: "200px" }}
+                    >
+                        <img src={icon} />
                     </div>
-                    <div className="pl-5 ml5" style={{borderLeft:"2px solid #0BA5ED"}}>
-
-                    <p><strong>{description}</strong></p>
+                    <div
+                        className="pl-5 ml5 d-flex align-content-center"
+                        style={{ borderLeft: "2px solid #FC9200" }}
+                    >
+                        <p>
+                            <strong>{description}</strong>
+                        </p>
                     </div>
                 </div>
             </section>
-    </Layout>
+        </Layout>
+    )
 }
--- src/pages/SeperationPage.js
+++ src/pages/SeperationPage.js
@@ -50,15 +50,30 @@ import SEO from "../components/seo"
                 }
         `)
     
-
+console.log(allPrismicSeperation)
         let description =
-            allPrismicSeperation.edges[0].node.data.description.text
+            allPrismicSeperation.edges[0].node.data.description[0].text
         let header_image =
-            allPrismicSeperation.edges[0].node.data.header_image.text   
+            allPrismicSeperation.edges[0].node.data.header_image.url   
         let icon = allPrismicSeperation.edges[0].node.data.icon.url
-        let title = allPrismicSeperation.edges[0].node.data.title.text
+        let title = allPrismicSeperation.edges[0].node.data.title[0].text
         let separation_products = allPrismicSeperation.edges[0].node.data.separation_products
     
-        return <Layout></Layout>
+        return <Layout>
+            <div className="background" style={{ background: `url(${header_image})`, backgroundSize:"cover",height:"60vh" }}></div>
+            <section className="container">
+                <h1 style={{color:"#962C17"}}><strong>{title}</strong></h1>
+                <div className="d-flex mt-5">
+                    <div className="imgContainer mr-5" style={{width:"200px"}}>
+
+                    <img src={icon}/>
+                    </div>
+                    <div className="pl-5 ml5 d-flex align-content-center" style={{borderLeft:"2px solid #962C17"}}>
+
+                       <p><strong>{description}</strong></p>
+                    </div>
+                </div>
+            </section>
+        </Layout>
     }
     
