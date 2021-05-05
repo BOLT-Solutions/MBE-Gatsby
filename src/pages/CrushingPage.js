77d37edc20c0d68fbfb3403faa2711a9196c84d4
Amr.Moushtaha <Amr.Moushtaha@BOLT-MOUSHTAHA>
2021-05-05 13:01:51 +0200

    Update CrushingPage.js

--- src/pages/CrushingPage.js
+++ src/pages/CrushingPage.js
@@ -57,8 +57,12 @@ export default function CrushingPage() {
     let title = allPrismicCrushingandgrinding.edges[0].node.data.title[0].text
     let products = allPrismicCrushingandgrinding.edges[0].node.data.products
 
+    console.log("******Products*******")
+    console.log(products);
+
     return (
         <Layout>
+
             <div className="background" style={{ background: `url(${banner_image})`, backgroundSize:"cover",height:"60vh" }}></div>
             <section className="container">
                 <h1><strong>{title}</strong></h1>
@@ -73,6 +77,34 @@ export default function CrushingPage() {
                     </div>
                 </div>
             </section>
+
+
+            <section className="container">
+
+        {
+            products.map( product => ( 
+                <div className="row">              
+                    <h1 className="ml-1" style={{fontSize:'40px'}}><strong>{product.product_title[0].text}</strong></h1>
+                    <div className="d-inline-flex mt-5 row justify-content-around">
+                        <div className="imgContainer mr-5 col-5">
+                            <img  src={product.product_image.url} style ={ width: '100%'}/>
+                        </div>
+                        <div className="pl-5 ml5 col-6">
+                            <h2 style={{fontSize:'30px'}}><strong>{product.product_title[0].text}</strong></h2>
+                            <p style={{fontSize:'14px' , minHeight:'300px'}}><strong>{product.product_description[0].text}</strong></p>
+                            <div className="d-flex justify-content-between" style={{width:'100%'}}>
+                                <a style={{color:'#0BA5ED', fontSize:'17px'}}  href= {product.download_brochure_link.url}  ><strong>Download brochure</strong></a>
+                                <a style={{color:'#0BA5ED', fontSize:'17px'}}  href= {product.request_info_link.url} ><strong>Request information</strong></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))
+        }
+
+
+            </section>
+
         </Layout>
     )
 }
