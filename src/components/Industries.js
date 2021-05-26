f9bf40515adabdb2a402c550e711724df63d8c6d
y.ghazy@boltsmartsolutions.com <y.ghazy@boltsmartsolutions.com>
2021-05-26 11:33:34 +0200

    fix bugs

--- src/components/Industries.js
+++ src/components/Industries.js
@@ -1,150 +1,158 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { graphql, Link, useStaticQuery } from "gatsby"
-import industies4 from '../assets/img/Icons/industies4.png';
-import industies5 from '../assets/img/Icons/industies5.png';
-import industies6 from '../assets/img/Icons/industies6.png';
-import AdobeStockPreview from '../assets/img/AdobeStock_219100685_Preview.jpeg';
+import industies4 from "../assets/img/Icons/industies4.png"
+import industies5 from "../assets/img/Icons/industies5.png"
+import industies6 from "../assets/img/Icons/industies6.png"
+import AdobeStockPreview from "../assets/img/AdobeStock_219100685_Preview.jpeg"
 
 export default function Industries() {
-const {allPrismicIndustries} = useStaticQuery(graphql`
-query Industries {
-    allPrismicIndustries {
-    edges {
-      node {
-        data {
-          banner_image {
-            url
-          }
-          description {
-            text
-          }
-          industries {
-            first_paragph {
-              text
+  const { allPrismicIndustries } = useStaticQuery(graphql`
+    query Industries {
+      allPrismicIndustries {
+        edges {
+          node {
+            data {
+              banner_image {
+                url
+              }
+              description {
+                text
+              }
+              industries {
+                first_paragph {
+                  text
+                }
+                industry_icon {
+                  url
+                }
+                industry_title {
+                  text
+                }
+                request_info_link {
+                  url
+                }
+                second_paragraph {
+                  text
+                }
+                see_more_link {
+                  url
+                }
+                see_more_paragraph {
+                  text
+                }
+              }
+              subtitle {
+                text
+              }
+              title {
+                text
+              }
             }
-            industry_icon {
-              url
-            }
-            industry_title {
-              text
-            }
-            request_info_link {
-              url
-            }
-            second_paragraph {
-              text
-            }
-            see_more_link {
-              url
-            }
-            see_more_paragraph {
-              text
-            }
-          }
-          subtitle {
-            text
-          }
-          title {
-            text
           }
         }
       }
     }
+  `)
+  let data = allPrismicIndustries.edges[0].node.data
+  let banner_image = allPrismicIndustries.edges[0].node.data.banner_image.url
+  let description = allPrismicIndustries.edges[0].node.data.description.text
+  let subtitle = allPrismicIndustries.edges[0].node.data.subtitle.text
+  let title = allPrismicIndustries.edges[0].node.data.title.text
+  let content_industries = allPrismicIndustries.edges[0].node.data.industries
+
+  let seeMore = []
+  const [flag, setFlag] = React.useState([])
+  for (let i = 0; i < content_industries.length; i++) {
+    seeMore[i] = "false"
   }
-}
 
- `)
-let data =allPrismicIndustries.edges[0].node.data
-let banner_image =allPrismicIndustries.edges[0].node.data.banner_image.url
-let description =allPrismicIndustries.edges[0].node.data.description.text
-let subtitle =allPrismicIndustries.edges[0].node.data.subtitle.text
-let title =allPrismicIndustries.edges[0].node.data.title.text    
-let content_industries=allPrismicIndustries.edges[0].node.data.industries
+  const seeMoreContent = i => {
+    seeMore[i] = "true"
+    setFlag(seeMore)
+  }
+  const seeLessContent = i => {
+    seeMore[i] = "false"
+    setFlag(seeMore)
+  }
 
-    let seeMore = [];
-    const [flag, setFlag] = React.useState([]);
-    for (let i = 0; i < content_industries.length; i++) {
-        seeMore[i] = 'false'
+  return (
+    <>
+      <section
+        id="industries"
+        className="d-flex align-items-center hero2"
+        style={{ background: `url(${banner_image})` }}
+      >
+        <div className="container" data-aos="zoom-out" data-aos-delay="100">
+          <h1>
+            {" "}
+            <span>{title}</span>
+          </h1>
+          <h2>{subtitle}</h2>
+          <p>{description} </p>
+        </div>
+      </section>
 
-    };
-
-    const seeMoreContent = (i) => {
-        seeMore[i] = 'true'
-        setFlag(seeMore)
-
-    };
-    const seeLessContent = (i) => {
-        seeMore[i] = 'false'
-        setFlag(seeMore)
-    };
-
-
-    return (
-
-    < >
-             <section id="industries" className="d-flex align-items-center hero2" style={{ background: `url(${banner_image})` }}>
-                    <div className="container" data-aos="zoom-out" data-aos-delay="100">
-                        <h1> <span>{title}</span>
-                        </h1>
-                        <h2>{subtitle}</h2>
-                        <p>{description} </p>
+      <section id="featured-services" className="featured-services">
+        <div className="container" data-aos="fade-up">
+          <div className="row align-items-baseline">
+            {content_industries.map((content_industry, i) => (
+              <div className="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+                <div
+                  className="icon-box d-flex flex-column justify-content-between"
+                  data-aos="fade-up"
+                  data-aos-delay="100"
+                >
+                  <div>
+                    <h4 className="title">
+                      <a href="">{content_industry.industry_title.text} </a>
+                    </h4>
+                    <div className="icon">
+                      <img
+                        src={content_industry.industry_icon.url}
+                        className="icoImage"
+                      />
                     </div>
-                </section>
+                    <p className="description" style={{ fontSize: "13px" }}>
+                      <span>{content_industry.first_paragph.text}</span>
+                      <span> {content_industry.second_paragraph.text}</span>
+                      {flag[i] == "true" && (
+                        <p style={{ lineHeight: "1.5",fontSize: "13px" }}>
+                      <span>{content_industry.see_more_paragraph.text}</span>    
+                        </p>
+                      )}
+                    </p>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <p className="request mb-0"  style={{ fontSize: "15px" }}> Request more Information</p>
 
-                <section id="featured-services" className="featured-services">
-                    <div className="container" data-aos="fade-up">
-
-                        <div className="row align-items-baseline">
-                        {
-                           content_industries.map((content_industry ,i)=> (
-                             <div className="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
-                                   <div className="icon-box d-flex flex-column justify-content-between" data-aos="fade-up" data-aos-delay="100">
-                                       <div>
-                                  <h4 className="title"><a href="">{content_industry.industry_title.text} </a></h4>
-                                  <div className="icon">
-                                      <img src={content_industry.industry_icon.url} className="icoImage" />
-                                  </div>
-                                  <p className="description" style={{fontSize:'13px'}}>
-                                      <span>
-                                          {content_industry.first_paragph.text}
-                                       </span> 
-                                               {content_industry.second_paragraph.text}
-                                               {
-                                                   flag[i] == 'true' &&
-                                                   <p style={{ lineHeight:"1.5" }}>
-                                                       {content_industry.see_more_paragraph.text}
-                                                   </p>
-                                               }
-                                    </p>
-                                       </div>
-                                       <div className="d-flex justify-content-between">
-                                           <p className="request mb-0">Request more Information
-                                           </p>
-
-                                           {
-                                               flag[i] != 'true' &&
-                                               <p className="request mb-0" onClick={() => { seeMoreContent(i) }}>See more
-                                           </p>
-                                           }
-                                           {
-                                               flag[i] == 'true' &&
-                                               <p className="request mb-0" onClick={() => { seeLessContent(i) }}>See less
-                                           </p>
-                                           }
-                                       </div>
-                              </div>
-                          </div>
-                            ))
-                        }
-                         
-                      </div>
-
-                    </div>
-                </section>
-
+                    {flag[i] != "true" && (
+                      <p  style={{ fontSize: "15px" }}
+                        className="request mb-0"
+                        onClick={() => {
+                          seeMoreContent(i)
+                        }}
+                      >
+                        See more
+                      </p>
+                    )}
+                    {flag[i] == "true" && (
+                      <p  style={{ fontSize: "15px" }}
+                        className="request mb-0"
+                        onClick={() => {
+                          seeLessContent(i)
+                        }}
+                      >
+                        See less
+                      </p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
-)}
-
-
-
+  )
+}
--- src/components/LifeCycle.js
+++ src/components/LifeCycle.js
@@ -1,148 +1,151 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { graphql, Link, useStaticQuery } from "gatsby"
-import solution1 from '../assets/img/Icons/solution1.png';
-import solution2 from '../assets/img/Icons/solution2.png';
-import solution3 from '../assets/img/Icons/solution3.png';
-import AdobeStockPreview2 from '../assets/img/AdobeStock_383254182_Preview.jpeg';
+import solution1 from "../assets/img/Icons/solution1.png"
+import solution2 from "../assets/img/Icons/solution2.png"
+import solution3 from "../assets/img/Icons/solution3.png"
+import AdobeStockPreview2 from "../assets/img/AdobeStock_383254182_Preview.jpeg"
 
-    
 export default function LifeCycle() {
-    const {allPrismicLifecycle} = useStaticQuery(graphql`
+  const { allPrismicLifecycle } = useStaticQuery(graphql`
     query Lifecycle {
       allPrismicLifecycle {
-    edges {
-      node {
-        data {
-          banner_image {
-            url
-          }
-          description {
-            text
-          }
-          subtitle {
-            text
-          }
-          support_options {
-            request_info_link {
-              url
+        edges {
+          node {
+            data {
+              banner_image {
+                url
+              }
+              description {
+                text
+              }
+              subtitle {
+                text
+              }
+              support_options {
+                request_info_link {
+                  url
+                }
+                see_more_link {
+                  url
+                }
+                see_more_paragraph {
+                  text
+                }
+                support_option_description {
+                  text
+                }
+                support_option_icon {
+                  url
+                }
+                support_option_title {
+                  text
+                }
+              }
+              title {
+                text
+              }
             }
-            see_more_link {
-              url
-            }
-            see_more_paragraph {
-              text
-            }
-            support_option_description {
-              text
-            }
-            support_option_icon {
-              url
-            }
-            support_option_title {
-              text
-            }
-          }
-          title {
-            text
           }
         }
       }
     }
+  `)
+
+  let banner_image = allPrismicLifecycle.edges[0].node.data.banner_image.url
+  let description = allPrismicLifecycle.edges[0].node.data.description.text
+  let subtitle = allPrismicLifecycle.edges[0].node.data.subtitle.text
+  let title = allPrismicLifecycle.edges[0].node.data.title.text
+  let content_cycle = allPrismicLifecycle.edges[0].node.data.support_options
+
+  let seeMore = []
+  const [flag, setFlag] = React.useState([])
+  for (let i = 0; i < content_cycle.length; i++) {
+    seeMore[i] = "false"
   }
-}
 
+  const seeMoreContent = i => {
+    seeMore[i] = "true"
+    setFlag(seeMore)
+  }
+  const seeLessContent = i => {
+    seeMore[i] = "false"
+    setFlag(seeMore)
+  }
 
+  return (
+    <>
+      <section
+        id="lifecycle"
+        className="d-flex align-items-center hero2"
+        style={{ background: `url(${banner_image})` }}
+      >
+        <div className="container" data-aos="zoom-out" data-aos-delay="100">
+          <h1> {title}</h1>
+          <h2>{subtitle}</h2>
+          <p>{description}</p>
+        </div>
+      </section>
 
-     `)
-
-    let banner_image = allPrismicLifecycle.edges[0].node.data.banner_image.url
-    let description = allPrismicLifecycle.edges[0].node.data.description.text
-    let subtitle = allPrismicLifecycle.edges[0].node.data.subtitle.text
-    let title = allPrismicLifecycle.edges[0].node.data.title.text    
-    let content_cycle = allPrismicLifecycle.edges[0].node.data.support_options
-
-    let seeMore = [];
-    const [flag, setFlag] = React.useState([]);
-    for (let i = 0; i < content_cycle.length; i++) {
-        seeMore[i] = 'false'
-
-    };
-
-    const seeMoreContent = (i) => {
-        seeMore[i] = 'true'
-        setFlag(seeMore)
-
-    };
-    const seeLessContent = (i) => {
-        seeMore[i] = 'false'
-        setFlag(seeMore)
-    };
-
-
-    return (
-
-    < >
-     <section id="lifecycle" className="d-flex align-items-center hero2" style={{ background: `url(${banner_image})` }} >
-                    <div className="container" data-aos="zoom-out" data-aos-delay="100">
-                        <h1> {title}
-                        </h1>
-                        <h2>{subtitle}</h2>
-                        <p>
-                            {description}
+      <section id="featured-services" className="featured-services">
+        <div className="container" data-aos="fade-up">
+          <div className="row">
+            {content_cycle.map((cycle, i) => (
+              <div className="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
+                <div
+                  className="icon-box  d-flex flex-column justify-content-between"
+                  data-aos="fade-up"
+                  data-aos-delay="100"
+                >
+                  <div>
+                    <h4 className="title">
+                      <a href="">{cycle.support_option_title.text} </a>
+                    </h4>
+                    <div className="icon">
+                      <img
+                        src={cycle.support_option_icon.url}
+                        className="icoImage"
+                      />
+                    </div>
+                    <p className="description">
+                      <span>{cycle.support_option_description.text}</span>
+                      {flag[i] == "true" && (
+                        <p style={{ lineHeight: "1.5", fontSize: "13px"  }}>
+                         <span> {cycle.see_more_paragraph.text}</span>
                         </p>
-                    </div>
-                </section>
+                      )}
+                    </p>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <p className="request mb-0"  style={{ fontSize: "15px" }}>Request more Information</p>
 
-                <section id="featured-services" className="featured-services">
-                    <div className="container" data-aos="fade-up">
-
-                        <div className="row">
-                        {
-                           content_cycle.map((cycle ,i)=> (
-                             <div className="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0">
-                                   <div className="icon-box  d-flex flex-column justify-content-between" data-aos="fade-up" data-aos-delay="100">
-                                       <div>
-                                  <h4 className="title"><a href="">{cycle.support_option_title.text} </a></h4>
-                                  <div className="icon">
-                                      <img src={cycle.support_option_icon.url} className="icoImage" />
-                                  </div>
-                                  <p className="description">
-                                      <span>
-                                          {cycle.support_option_description.text}
-                                       </span> 
-                                           {
-                                               flag[i] == 'true' &&
-                                               <p style={{ lineHeight: "1.5" }}>
-                                                       {cycle.see_more_paragraph.text}
-                                               </p>
-                                           }
-                                       </p>
-                                   </div>
-                                   <div className="d-flex justify-content-between">
-                                       <p className="request mb-0">Request more Information
-                                           </p>
-
-                                       {
-                                           flag[i] != 'true' &&
-                                           <p className="request mb-0" onClick={() => { seeMoreContent(i) }}>See more
-                                           </p>
-                                       }
-                                       {
-                                           flag[i] == 'true' &&
-                                           <p className="request mb-0" onClick={() => { seeLessContent(i) }}>See less
-                                           </p>
-                                       }
-                                   </div>
-                              </div>
-                          </div>
-                            ))
-                        }
-                            
-                        </div>
-
-                    </div>
-                </section>
+                    {flag[i] != "true" && (
+                      <p  style={{ fontSize: "15px" }}
+                        className="request mb-0"
+                        onClick={() => {
+                          seeMoreContent(i)
+                        }}
+                      >
+                        See more
+                      </p>
+                    )}
+                    {flag[i] == "true" && (
+                      <p  style={{ fontSize: "15px" }}
+                        className="request mb-0"
+                        onClick={() => {
+                          seeLessContent(i)
+                        }}
+                      >
+                        See less
+                      </p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
-)}
-
+  )
+}
