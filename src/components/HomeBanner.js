0ed8520a0ac826cd4ba7c0c16f87de4391c925d6
Amr.Moushtaha <amrmoushtaha@gmail.com>
2021-05-30 14:18:01 +0200

    Updated homepage SEO

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -7,10 +7,11 @@ import slide2 from '../assets/img/slide/slide-2.jpg';
 import slide3 from '../assets/img/slide/slide-3.jpg';
 import "./layout.css"
 import mbeText16 from '../assets/img/MBE-text-16.png';
+import { Helmet } from "react-helmet"
 
 
 export default function HomeBanner() {
-    const {allPrismicHomeheader} = useStaticQuery(graphql`
+    const { allPrismicHomeheader } = useStaticQuery(graphql`
     query HomeBanner {
         allPrismicHomeheader {
             edges {
@@ -27,115 +28,146 @@ export default function HomeBanner() {
                       url
                     }
                   }
+                  meta_tags {
+                    meta_content {
+                      text
+                    }
+                    meta_name {
+                      text
+                    }
+                  }
+                  page_title {
+                    text
+                  }
                 }
               }
             }
           }
         }
         `)
-    let image_carousel =allPrismicHomeheader.edges[0].node.data.image_carousel
 
-    console.log(image_carousel );
+    let image_carousel = allPrismicHomeheader.edges[0].node.data.image_carousel
+    let page_title = allPrismicHomeheader.edges[0].node.data.page_title[0].text
+    let meta_tags = allPrismicHomeheader.edges[0].node.data.meta_tags
 
-let slideIndex = 1
+    console.log(image_carousel);
 
-const plusDivs = (n) => {
-    console.log(n, slideIndex)
-    showDivs(slideIndex += n);
-};
-const currentDiv = (n) => {
-    console.log(n)
+    let slideIndex = 1
 
-    showDivs(slideIndex = n);
-};
-const showDivs = (n) => {
+    const plusDivs = (n) => {
+        console.log(n, slideIndex)
+        showDivs(slideIndex += n);
+    };
+    const currentDiv = (n) => {
+        console.log(n)
 
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    if (n > x.length) {
-        slideIndex = 1;
+        showDivs(slideIndex = n);
+    };
+    const showDivs = (n) => {
+
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = x.length;
+        }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" w3-grey", "");
+        }
+        console.log("test", x[slideIndex - 1])
+        if (x[slideIndex - 1] !== undefined) {
+
+            x[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " w3-grey";
+        }
     }
-    if (n < 1) {
-        slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-grey", "");
-    }
-    console.log("test", x[slideIndex - 1])
-    if (x[slideIndex - 1] !== undefined) {
-
-        x[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " w3-grey";
-    }
-}
-useEffect(() => {
+    useEffect(() => {
 
         showDivs(slideIndex);
- });
+    });
     return (
 
         <>
-        <section id="hero" className="hero homeBanner" >
+
+            <Helmet>
+                {
+                    meta_tags.map(meta_tag => (
+                        <meta name={meta_tag.meta_name[0].text} content={meta_tag.meta_content[0].text}></meta>
+                    ))
+
+
+
+                }
+
+                <title>{page_title}</title>
+
+            </Helmet>
+
+
+            <section id="hero" className="hero homeBanner" >
                 <div className="container-fluid">
-                          <div className="row">
-                              <div className="col-md-5 col-lg-5">
-                                  <div className="main_logo">
-                                      <img src={logo} style={{ width: '75%' ,  marginLeft:"15%" }} />
-                                  </div>
-                              </div>
-                              <div className="col-md-7 col-lg-7">
-  
-                                  <div className="slider">
-                                      <div className="w3-content w3-display-container">
-                                      {image_carousel.map(image => (
-                                        
-                                          <img className="mySlides" src={image.banner_image.url} style={{ width: '100%' }} />
-                                            ))
-                                          }
-                                          {/* <img className="mySlides" src={slide2} style={{ width: '100%' }} />
+                    <div className="row">
+                        <div className="col-md-5 col-lg-5">
+                            <div className="main_logo">
+                                <img src={logo} style={{ width: '75%', marginLeft: "15%" }} />
+                            </div>
+                        </div>
+                        <div className="col-md-7 col-lg-7">
+
+                            <div className="slider">
+                                <div className="w3-content w3-display-container">
+                                    {image_carousel.map(image => (
+
+                                        <img className="mySlides" src={image.banner_image.url} style={{ width: '100%' }} />
+                                    ))
+                                    }
+                                    {/* <img className="mySlides" src={slide2} style={{ width: '100%' }} />
                                           <img className="mySlides" src={slide3} style={{ width: '100%' }} /> */}
-                                          <div className="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style={{ width: '100%' }}>
-                                              {/* <div className="w3-left w3-hover-text-khaki" onClick={() => { plusDivs(-1) }}>&#10094;</div>
+                                    <div className="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style={{ width: '100%' }}>
+                                        {/* <div className="w3-left w3-hover-text-khaki" onClick={() => { plusDivs(-1) }}>&#10094;</div>
                                               <div className="w3-right w3-hover-text-khaki" onClick={() => { plusDivs(1) }}>&#10095;</div> */}
-                                              {image_carousel.map((image, index)  => (
-                                             
-                                        // <img className="mySlides" src={image.banner_image.url} style={{ width: '100%' }} />
-                                        <span className="w3-badge demo w3-border w3-transparent p-0 mx-1" onClick={() => { let n=index+1 ; currentDiv(n) }}></span>
-                                            ))
-                                           }
-                                          </div>
-                                      </div>
-                                  </div>
-  
-                              </div>
-                          </div>
+                                        {image_carousel.map((image, index) => (
+
+                                            // <img className="mySlides" src={image.banner_image.url} style={{ width: '100%' }} />
+                                            <span className="w3-badge demo w3-border w3-transparent p-0 mx-1" onClick={() => { let n = index + 1; currentDiv(n) }}></span>
+                                        ))
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-        </section>
-  
-        <section className="underHero">
-                      <div className=" container inner">
-                          <div className="row">
-                              <div className="col-md stext">
-                                  <img src={mbeText16} />
-  
-                              </div>
-                              <div className="col-md social-right">
-                                  <a className="link" href="#">
-                                      <i className="fab fa-linkedin-in"></i>
-                                  </a>
-                                  <a className="link" href="#">
-                                      <i className="fab fa-telegram"></i>
-                                  </a>
-                              </div>
-                          </div>
-  
-                      </div>
-        </section>
-      </>
-)}
+            </section>
+
+            <section className="underHero">
+                <div className=" container inner">
+                    <div className="row">
+                        <div className="col-md stext">
+                            <img src={mbeText16} />
+
+                        </div>
+                        <div className="col-md social-right">
+                            <a className="link" href="#">
+                                <i className="fab fa-linkedin-in"></i>
+                            </a>
+                            <a className="link" href="#">
+                                <i className="fab fa-telegram"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </>
+    )
+}
 
 
